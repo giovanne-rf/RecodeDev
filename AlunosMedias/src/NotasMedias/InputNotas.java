99536b930cbe8ebd705be99acba9db1fe1a58ff3
Giovanne <giovanne.alves@gmail.com>
2020-09-17 17:10:57 -0300

    Finalização

--- AlunosMedias/src/NotasMedias/InputNotas.java
+++ AlunosMedias/src/NotasMedias/InputNotas.java
@@ -7,12 +7,10 @@ class inputNotas {
     public static void main(String[] args) {
 
         Alunos aluno = new Alunos("Giovanne", 8, 9);
-        System.out.println(aluno.resultado(aluno.media));
 
-        System.out.println(aluno.media());
-        System.out.println(aluno.nota1);
-        System.out.println(aluno.nota2);
-        System.out.println(aluno.media);
+        System.out.println("Nota 1: "+ aluno.nota1);
+        System.out.println("Nota 2: " +aluno.nota2);
+        System.out.println("O aluno teve média " + aluno.media()+" e foi "+ aluno.resultado(aluno.media()));
 
 
     }
