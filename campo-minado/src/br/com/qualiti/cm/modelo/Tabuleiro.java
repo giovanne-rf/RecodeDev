c946c4480701ae78dd32a1879156e47a82476607
Giovanne <giovanne.alves@gmail.com>
2020-09-25 19:13:31 -0300

    Implementação do Tabuleiro

---
+++ campo-minado/src/br/com/qualiti/cm/modelo/Tabuleiro.java
@@ -0,0 +1,63 @@
+package br.com.qualiti.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+    private final List<Campo>  campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+        gerarCampos();
+        associarOsVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos(){
+        for (int linha=0; linha<linhas; linha++){
+            for (int coluna=0; coluna<colunas; coluna++){
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarOsVizinhos() {
+        for (Campo c1: campos){
+            for (Campo c2: campos){
+            c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+        long minhasArmadas = 0;
+        Predicate<Campo> minado = c-> c.isMinado();
+        do {
+            minhasArmadas = campos.stream().filter(minado).count();
+            int aleatorio =(int) (Math.random()* campos.size()); // * PRIORIDADE DO CAST
+            campos.get(aleatorio).minar();
+        } while (minhasArmadas<minas);
+    }
+
+    public boolean objetivoAlcancado (){
+        return  campos.stream().allMatch(c-> c.objetivoAlcancado());
+    }
+
+    public void reiniciar(){
+        campos.stream().forEach(c->c.reiniciar());
+        sortearMinas();
+    }
+
+    public String toString (){
+
+    }
+
+
+
+}
--- campo-minado/src/br/com/qualiti/cm/modelo/tabuleiro.java
+++
@@ -1,6 +0,0 @@
-package br.com.qualiti.cm.modelo;
-
-public class tabuleiro {
-
-
-}
