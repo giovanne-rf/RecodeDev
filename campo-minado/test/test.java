7ffc0e4e5acca604f390aa301cf5187e3887edc3
Giovanne <giovanne.alves@gmail.com>
2020-09-25 10:20:47 -0300

    projeto campo-minado.iml

---
+++ campo-minado/test/test.java
@@ -0,0 +1,12 @@
+
+
+import static org.testng.FileAssert.fail;
+import  org.junit.jupiter.api.Assertions.*;
+
+public class test {
+
+    void test(){
+        fail ("not yet implement");
+
+    }
+}
---
+++ campo-minado/test/testTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.FileAssert.fail;
+
+class testTest {
+
+    void test(){
+        fail ("not yet implement");
+    }
+
+
+}
