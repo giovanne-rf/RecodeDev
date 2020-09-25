4b0004a0bb4d77be21fb3c3c0dd37c12cdffab2b
Giovanne <giovanne.alves@gmail.com>
2020-09-25 14:31:03 -0300

    Testes de vizinhos

--- campo-minado/test/br/com/qualiti/cm/modelo/CampoTest.java
+++ campo-minado/test/br/com/qualiti/cm/modelo/CampoTest.java
@@ -1,18 +1,58 @@
 package br.com.qualiti.cm.modelo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CampoTest {
 
-    private Campo campo = new Campo(3, 3);
+    private Campo campo;
+
+    @BeforeEach
+    void iniciarCampo (){
+        campo = new Campo( 3, 3);
+    }
 
     @Test
-    void adicionarVizinho() {
+    void adicionarVizinhoEsquerda() {
         Campo vizinho = new Campo(3, 2);
         boolean resultado = campo.adicionarVizinho(vizinho);
-
         assertTrue(resultado);
     }
+    @Test
+    void adicionarVizinhoDireita() {
+        Campo vizinho = new Campo(3, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+        void adicionarVizinhoCima() {
+        Campo vizinho = new Campo(2,3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+    @Test
+        void adicionarVizinhoBaixo() {
+        Campo vizinho = new Campo(4,3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+        void adicionarDiagonal() {
+        Campo vizinho = new Campo(2,2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    void adicionarNaoVizinho() {
+        Campo vizinho = new Campo(5,2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertFalse(resultado);
+    }
+
+
 }
