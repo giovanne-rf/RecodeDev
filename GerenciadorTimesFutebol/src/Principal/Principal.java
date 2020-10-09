6c7f2a5044bd8b761284eb75f47ac520f578af66
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-09 07:13:57 -0300

    Implementação da interface

--- GerenciadorTimesFutebol/src/Principal/Principal.java
+++ GerenciadorTimesFutebol/src/Principal/Principal.java
@@ -1,12 +1,12 @@
 package Principal;
 
-import times.incluirTime;
+import times.gerenciadorTime;
 
 public class Principal {
 
     public static void main(String[] args) {
 
-        incluirTime novoTime = new incluirTime();
+        gerenciadorTime novoTime = new gerenciadorTime();
         novoTime.buscarTimes();
     }
 }
---
+++ GerenciadorTimesFutebol/src/times/gerenciadorTime.java
@@ -0,0 +1,106 @@
+package times;
+
+import MeuTimeInterface.MeuTimeInterface;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public class gerenciadorTime implements MeuTimeInterface {
+
+    private Long idTime;
+    private String nomeTime;
+    private LocalDate dataCriacao;
+    private String corUniformePrincipal;
+    private String corUniformeSecundario;
+
+    Long idJogador;
+    String nomeJogador;
+    LocalDate dataNascimento;
+    Integer nivelHabilidade;
+    BigDecimal salario;
+    Boolean capitao;
+
+    @Override
+    public void incluirTime(Long id,
+                            String nome,
+                            LocalDate dataCriacao,
+                            String corUniformePrincipal,
+                            String corUniformeSecundario) {
+        this.idTime =  id;
+        this.nomeTime = nome;
+        this.dataCriacao=dataCriacao;
+        this.corUniformePrincipal = corUniformePrincipal;
+        this.corUniformeSecundario = corUniformeSecundario;
+    }
+
+    @Override
+    public void incluirJogador(Long idJogador,
+                               Long idTime,
+                               String nomeJogador,
+                               LocalDate dataNascimento,
+                               Integer nivelHabilidade,
+                               BigDecimal salario) {
+        this.idJogador = idJogador;
+        this.idTime = idTime;
+        this.nomeJogador = nomeJogador;
+        this.dataNascimento = dataNascimento;
+        this.nivelHabilidade = nivelHabilidade;
+        this.salario = salario;
+    }
+
+    @Override
+    public void definirCapitao(Long idJogador) {
+
+    }
+
+    @Override
+    public Long buscarCapitaoDoTime(Long idTime) {
+        return null;
+    }
+
+    @Override
+    public String buscarNomeJogador(Long idJogador) {
+        return null;
+    }
+
+    @Override
+    public String buscarNomeTime(Long idTime) {
+        return null;
+    }
+
+    @Override
+    public Long buscarJogadorMaiorSalario(Long idTime) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal buscarSalarioDoJogaodor(Long idJogador) {
+        return null;
+    }
+
+    @Override
+    public List<Long> buscarJogadoresDoTime(Long idTime) {
+        return null;
+    }
+
+    @Override
+    public Long buscarMelhorJogadorDoTime(Long idTime) {
+        return null;
+    }
+
+    @Override
+    public Long buscarJogadorMaisVelho(Long idTime) {
+        return null;
+    }
+
+    @Override
+    public List<Long> buscarTimes() {
+        return null;
+    }
+
+    @Override
+    public List<Long> buscarTopJogadores(Integer top) {
+        return null;
+    }
+}
