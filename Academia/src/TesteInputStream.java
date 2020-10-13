33f7fa935a152a54529601ee99babc5436fd01c6
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-13 16:37:16 -0300

    Teste InputStream

---
+++ Academia/src/TesteInputStream.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.sql.SQLOutput;
+
+public class TesteInputStream {
+    public static void main(String[] args) {
+        try{
+            byte mbyte [] = new byte [100];
+            System.out.println("Digite sua idade:");
+            System.in.read(mbyte);
+            String mString = new String (mbyte).trim();
+            int idade = Integer.parseInt(mString);
+            System.out.println("Idade convertida em int" + idade);
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Erro de conversao");;
+        }
+    }
+}
