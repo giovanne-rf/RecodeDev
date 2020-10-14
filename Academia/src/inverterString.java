34c5e2ae85c2b02168ecac458cf56e14f233b05d
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-14 17:23:26 -0300

    Nome invertido

--- Academia/src/inverterString.java
+++ Academia/src/inverterString.java
@@ -3,11 +3,14 @@ public class inverterString {
     public static void main(String[] args) {
 
         String nome = new String("Giovanne José Alves Pereira");
+        String nomeInvertido = "";
 
         System.out.println(nome);
 
-        for (int i=0; i<nome.length(); i++){
-            System.out.println(nome);
+        for (int i=nome.length()-1; i>-1; i--){
+            nomeInvertido += nome.charAt(i);
         }
+
+        System.out.println(nomeInvertido);
     }
 }
