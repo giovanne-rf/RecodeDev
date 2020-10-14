c30be3198ea41afa45f22e665817d7f7148d3681
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-14 16:52:30 -0300

    Switch Case

---
+++ Academia/src/Strings.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class Strings {
+
+
+    public static void main(String[] args) {
+
+        int mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do mês: "));
+        switch (mes) {
+            case 1:case 2: case 3:
+            System.out.println("Verão");
+            break;
+
+            case 4:  case 5:  case 6:
+                System.out.println("Primavera");
+                break;
+
+            case 7:  case 8:   case 9:
+                System.out.println("Inverno");
+                break;
+
+            case 10: case 11:            case 12:
+                System.out.println("Outuno");
+                break;
+            default:
+
+                System.out.println("Digite um mês valido");
+                break;
+        }
+//        System.out.println("A estação do ano é " + mes);
+    }
+}
