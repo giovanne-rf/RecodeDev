8b6a91c9a1e895b6ee6710e13da062380c3506d9
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-14 16:59:39 -0300

    Strings replace and indexOf

---
+++ Academia/src/Strings2.java
@@ -0,0 +1,10 @@
+public class Strings2 {
+
+    public static void main(String[] args) {
+
+        String frase = "A casa caiu amigos!";
+        System.out.println(frase.indexOf("s"));
+        System.out.println(frase.lastIndexOf("s"));
+        System.out.println(frase.replace("s", " "));
+    }
+}
