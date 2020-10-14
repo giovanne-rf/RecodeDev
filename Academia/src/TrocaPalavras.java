106390f68424637fdbb147e0afe5123f824d3f3a
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-14 17:12:25 -0300

    Strings replace and indexOf

---
+++ Academia/src/TrocaPalavras.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class TrocaPalavras {
+
+    public static void main(String[] args) {
+
+        String frase = JOptionPane.showInputDialog("Digite a frase: ");
+        String troca = JOptionPane.showInputDialog("Qual caracter você quer trocar: ");
+        String novaparte =  JOptionPane.showInputDialog("Nova parte: ");
+
+        System.out.println(frase.replace(troca, novaparte));
+
+    }
+}
