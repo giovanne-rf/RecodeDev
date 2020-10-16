bc55024faab1b444e7e943893d167eb8c8821ccc
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-16 14:48:38 -0300

    Calculadora

---
+++ Academia/src/Calculadora/Calculadora.java
@@ -0,0 +1,12 @@
+package Calculadora;
+
+import Calculadora.Soma.Soma;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Soma soma = new Soma (52, 369);
+        System.out.println(soma.Resultado());
+        ;
+    }
+}
---
+++ Academia/src/Calculadora/Soma/Soma.java
@@ -0,0 +1,17 @@
+package Calculadora.Soma;
+
+public class Soma {
+
+    double entrada1;
+    double entrada2;
+    double resultado;
+
+    public Soma(double entrada1, double entrada2) {
+        this.entrada1 = entrada1;
+        this.entrada2 = entrada2;
+    }
+
+    public double Resultado (){
+        return resultado = entrada1+entrada2;
+    }
+}
