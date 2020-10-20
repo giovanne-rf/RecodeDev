cd9d44bd7d7c54d16f8af1b24790fde07fe492fb
Giovanne <giovanne.alves@gmail.com>
2020-10-20 10:14:23 -0300

    JFrame 2

--- Academia/src/Interfaces/TesteJFrame.java
+++ Academia/src/Interfaces/TesteJFrame.java
@@ -13,12 +13,30 @@ public class TesteJFrame extends JFrame {
 
         getContentPane().setLayout(null);
 
-        getContentPane().setBackground(new Color(255,255,255));
+        getContentPane().setBackground(new Color(155,200,100));
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    public void centralizar (){
+        Dimension screen =  Toolkit.getDefaultToolkit().getScreenSize();
+
+        Dimension janela  = getSize();
+
+        if (janela.height > screen.height){
+            setSize(janela.width, screen.height);
+
+        if (janela.width >screen.width){
+            setSize(screen.width, janela.width);
+            setLocation((screen.width - janela.width) / 2 ,(screen.height - janela.width) / 2);
+        }
+        }
     }
 
     public static void main(String[] args) {
         TesteJFrame janela = new TesteJFrame();
         janela.setVisible(true);
+        janela.centralizar();
     }
 }
