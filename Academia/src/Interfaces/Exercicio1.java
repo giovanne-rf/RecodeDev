a4b8e4f79e199451c6a597b6efe4ffae72db0e4c
Giovanne <giovanne.alves@gmail.com>
2020-10-20 16:19:20 -0300

    Exercicio JFrame

--- Academia/src/Interfaces/Exercicio1.java
+++ Academia/src/Interfaces/Exercicio1.java
@@ -7,18 +7,12 @@ public class Exercicio1 extends  JFrame{
 
 
     public static void main(String[] args) {
-
         Exercicio1 novaJanela = new Exercicio1();
- ;
         novaJanela.setVisible(true);
-        novaJanela.centralizar();
-
 
     }
 
-
     public Exercicio1 (){
-
         setTitle(JOptionPane.showInputDialog("Digite o título da janela"));
         int largura = Integer.parseInt(JOptionPane.showInputDialog("Digite a largura da janela"));
         int altura = Integer.parseInt(JOptionPane.showInputDialog("Digite a altura da janela"));
@@ -27,18 +21,13 @@ public class Exercicio1 extends  JFrame{
         int b = Integer.parseInt(JOptionPane.showInputDialog("Digite B"));
 
         setSize(largura, altura);
-
-
         getContentPane().setLayout(null);
-
         getContentPane().setBackground(new Color(r,g,b));
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void centralizar (){
         Dimension screen =  Toolkit.getDefaultToolkit().getScreenSize();
-
         Dimension janela  = getSize();
 
         if (janela.height > screen.height){
@@ -49,5 +38,20 @@ public class Exercicio1 extends  JFrame{
                 setLocation((screen.width - janela.width) / 2 ,(screen.height - janela.width) / 2);
             }
         }
+    }
+
+    public JLabel CriarJLabel(){
+        JLabel jl = new JLabel();
+        jl.setText("JLabel inserido");
+        jl.setLocation(10,10);
+        jl.setSize(370,50);
+        jl.setOpaque(true);
+        jl.setBackground(new Color(255, 255, 255));
+        jl.setForeground(new Color(0,0,0));
+        jl.setFont(new Font( "Courier new", Font.BOLD, 12));
+        jl.setToolTipText("JLabel Exemplo");
+        jl.setHorizontalAlignment(SwingConstants.LEFT);
+        jl.setVerticalAlignment(SwingConstants.TOP);
+        return jl;
     }
 }
--- Academia/src/Interfaces/TesteJFrame.java
+++ Academia/src/Interfaces/TesteJFrame.java
@@ -8,35 +8,53 @@ public class TesteJFrame extends JFrame {
     public TesteJFrame (){
 
         setTitle("Primeira Janela Swing");
-
         setSize(500,500);
-
         getContentPane().setLayout(null);
-
         getContentPane().setBackground(new Color(155,200,100));
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
-    public void centralizar (){
-        Dimension screen =  Toolkit.getDefaultToolkit().getScreenSize();
+    public void centralizar () {
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension janela = getSize();
 
-        Dimension janela  = getSize();
-
-        if (janela.height > screen.height){
+        if (janela.height > screen.height) {
             setSize(janela.width, screen.height);
 
-        if (janela.width >screen.width){
-            setSize(screen.width, janela.width);
-            setLocation((screen.width - janela.width) / 2 ,(screen.height - janela.width) / 2);
-        }
+            if (janela.width > screen.width) {
+                setSize(screen.width, janela.width);
+                setLocation((screen.width - janela.width) / 2, (screen.height - janela.width) / 2);
+            }
         }
     }
 
+        public JLabel CriarJLabel(){
+            JLabel jl = new JLabel();
+            jl.setText("JLabel inserido");
+            jl.setLocation(10,10);
+            jl.setSize(370,50);
+            JTextField jt = new JTextField();
+            jt.setToolTipText("Insira o texto");
+            jt.setHorizontalAlignment(JTextField.TOP);
+            jl.setOpaque(true);
+            jl.setBackground(new Color(255, 255, 255));
+            jl.setForeground(new Color(0,0,0));
+            jl.setFont(new Font( "Courier new", Font.BOLD, 12));
+            jl.setToolTipText("JLabel Exemplo");
+            jl.setHorizontalAlignment(SwingConstants.LEFT);
+            jl.setVerticalAlignment(SwingConstants.TOP);
+            return jl;
+
+        }
+
+
+
     public static void main(String[] args) {
         TesteJFrame janela = new TesteJFrame();
-        janela.setVisible(true);
-        janela.centralizar();
+        janela.setVisible(false);
+        janela.CriarJLabel();
+
+
     }
 }
