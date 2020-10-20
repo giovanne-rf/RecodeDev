dee5e14a3ab1e3a8e3507dbe7ee5a1e3bf69d61c
Giovanne <giovanne.alves@gmail.com>
2020-10-20 10:26:54 -0300

    Exercicio JFrame

---
+++ Academia/src/Interfaces/Exercicio1.java
@@ -0,0 +1,53 @@
+package Interfaces;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exercicio1 extends  JFrame{
+
+
+    public static void main(String[] args) {
+
+        Exercicio1 novaJanela = new Exercicio1();
+ ;
+        novaJanela.setVisible(true);
+        novaJanela.centralizar();
+
+
+    }
+
+
+    public Exercicio1 (){
+
+        setTitle(JOptionPane.showInputDialog("Digite o título da janela"));
+        int largura = Integer.parseInt(JOptionPane.showInputDialog("Digite a largura da janela"));
+        int altura = Integer.parseInt(JOptionPane.showInputDialog("Digite a altura da janela"));
+        int r = Integer.parseInt(JOptionPane.showInputDialog("Digite R"));
+        int g = Integer.parseInt(JOptionPane.showInputDialog("Digite G"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Digite B"));
+
+        setSize(largura, altura);
+
+
+        getContentPane().setLayout(null);
+
+        getContentPane().setBackground(new Color(r,g,b));
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void centralizar (){
+        Dimension screen =  Toolkit.getDefaultToolkit().getScreenSize();
+
+        Dimension janela  = getSize();
+
+        if (janela.height > screen.height){
+            setSize(janela.width, screen.height);
+
+            if (janela.width >screen.width){
+                setSize(screen.width, janela.width);
+                setLocation((screen.width - janela.width) / 2 ,(screen.height - janela.width) / 2);
+            }
+        }
+    }
+}
