56b6149fb49f14d37ca96015ae0cae9789ad450f
Giovanne Alves <giovanne.alves@gmail.com>
2020-10-22 15:46:13 -0300

    UsandoMenus2

--- Academia/src/GerenciadoresLayout/UsandoMenus.java
+++ Academia/src/GerenciadoresLayout/UsandoMenus.java
@@ -7,6 +7,8 @@ public class UsandoMenus extends JFrame implements ActionListener
 {	JMenuBar BarraMenu = new JMenuBar();
     JMenu Arquivo = new JMenu ("Arquivo"); 	JMenu Editar = new JMenu ("Editar");
     JMenuItem Novo = new JMenuItem ("Novo"); 	JMenuItem Salvar = new JMenuItem ("Salvar");
+    JMenuItem Abrir = new JMenuItem ("Abrir");
+    JMenuItem Recarregar = new JMenuItem ("Recarregar");
     JMenuItem Sair = new JMenuItem ("Sair");
     JTextField T1 = new JTextField("Aqui teremos as respostas");
 
@@ -16,8 +18,13 @@ public class UsandoMenus extends JFrame implements ActionListener
         setLocation(50,50);
         setResizable(false);
         setJMenuBar(BarraMenu);
+        Arquivo.setMnemonic('A');
+        Abrir.setMnemonic('b');
+        Novo.setMnemonic('N');
+        Recarregar.setMnemonic('e');
+        Sair.setMnemonic('S');
         BarraMenu.add(Arquivo);	BarraMenu.add(Editar);
-        Arquivo.add(Novo);	Arquivo.add(Salvar);	Arquivo.add(Sair);
+        Arquivo.add(Novo);	Arquivo.add(Salvar);Arquivo.add(Abrir);;Arquivo.add(Recarregar);	Arquivo.add(Sair);
         Novo.addActionListener(this);Salvar.addActionListener(this);Sair.addActionListener(this);
         getContentPane().add(T1);
     }
@@ -34,6 +41,12 @@ public class UsandoMenus extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e)
     { if (e.getSource()==Novo)
         T1.setText("Escolhido o item de Menu Novo");
+        if (e.getSource()==Abrir)
+            T1.setText("Escolhido o item de Menu Salvar");
+
+
+        if (e.getSource()==Recarregar)
+            T1.setText("Escolhido o item de Menu Salvar");
         if (e.getSource()==Salvar)
             T1.setText("Escolhido o item de Menu Salvar");
         if (e.getSource()==Sair)
